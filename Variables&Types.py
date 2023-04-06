7db5f328e02cc34fb6fdd74b264bc7ef6a72ac1c
J-724R <jjpernian@gmail.com>
2023-04-06 18:57:54 -0400

    Variables&Types

---
+++ Variables&Types.py
@@ -0,0 +1,24 @@
+Ryuk = "dog"
+Ora = "dog"
+principe = "cat"
+
+variable = False
+
+if ( False and variable ):
+  print("or operator < or >")
+elif( True and variable ):
+  print("and operator < and >")
+
+
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)

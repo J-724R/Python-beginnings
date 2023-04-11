5959993d5b7af5b5843bb6db2808d0e69f6de1a2
J-724R <jjpernian@gmail.com>
2023-04-10 23:28:19 -0400

    Basic strings opetations

---
+++ BasicStringOperations.py
@@ -0,0 +1,16 @@
+
+# JS slice method
+
+astring = "Hello world!"
+print(astring[3:7])
+
+# easy way to reverse a string
+#! The general form is [start:stop:step].
+
+astring = "Hello world!"
+print(astring[::-1])
+
+
+astring = "Hello world!"
+afewwords = astring.split(" ")
+print(afewwords)

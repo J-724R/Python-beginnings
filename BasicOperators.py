859c7f2b3259fb6a3daefd0da85091938f08564e
J-724R <jjpernian@gmail.com>
2023-04-10 23:00:45 -0400

    BasicOperators

---
+++ BasicOperators.py
@@ -0,0 +1,22 @@
+# Same as other languages
+# Have the common math herarchy 
+
+# also has the remainder
+reminder = 11%3
+print(reminder)
+
+#power operator is with double *
+square = 2 ** 2
+cube =2 ** 3
+print(square)
+print(cube)
+
+#strings can be multiplied to get sequences
+lostOfRyuks = "Ryuk" * 10
+print(lostOfRyuks)
+
+#it can also be used to operate concat and repeat n times lists
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+concatLists = even_numbers + odd_numbers
+print(concatLists)

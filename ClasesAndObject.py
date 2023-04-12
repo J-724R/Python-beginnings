e944d4395cd7bd1493b3bdf368dc8ecbfc32c5f6
J-724R <jjpernian@gmail.com>
2023-04-12 13:27:16 -0400

    ClasesAndObjects

---
+++ ClasesAndObject.py
@@ -0,0 +1,28 @@
+class MyClass:
+    foo = "foo"
+
+    def function(self):
+        print("This is a message inside the class.")
+ 
+myobjecx = MyClass
+myobjecy = MyClass
+
+# accessing variables
+
+myobjecx.foo
+myobjecy.foo = "My Ryuk"
+
+print(myobjecx.foo)
+print(f"Another object varible: {myobjecy.foo}")
+
+# Multiple different objects can be created from the same class
+
+# Is the same thing for accessign function
+
+
+# init()
+# The __init__() function, is a special function that is called when 
+# the class is being initiated. It's used for assigning values in a class.
+
+
+

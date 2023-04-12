54988d607d0cdf022071d9e4749b08a7d0adf52a
J-724R <jjpernian@gmail.com>
2023-04-12 12:25:58 -0400

    functions

---
+++ Functions.py
@@ -0,0 +1,25 @@
+# Istead of using brackets to scoope blocks python uses iddentation
+
+# function:
+#   1st block line
+#   2sd block line
+    # 1st line of a new block inside the other
+
+# Function declaration
+def my_function():
+  print("Ryuk is the best dog around")
+
+# Python 3.6 way of using strings literals
+# ! prefered one
+def dog_info(dogName, dogAge):
+  print(f"""
+    Dog's name: {dogName}
+    Dog's age: {dogAge}
+  """)
+
+print(dog_info("Ryuk",8))
+
+
+
+
+

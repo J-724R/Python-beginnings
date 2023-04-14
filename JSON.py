28ee7af748fa99d0253f096199eebb1ad162c798
J-724R <jjpernian@gmail.com>
2023-04-14 18:27:34 -0400

    Extra comments

---
+++ JSON.py
@@ -0,0 +1,11 @@
+import json
+
+# the #! json.loads 
+# Converts json to python
+# turns a string into a json object datastructure
+
+# ! json.dumps
+# Converts form python to json
+# methods takes an object and returns a String
+
+
--- Serialization.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# the #! json.loads 
-# mehtod turns a string into a json object datastructure
-
-# ! json.dumps
-# methods takes an object and returns a String
-
-

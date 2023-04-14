039e98a26e6431c71ad0030487b7025f96611f85
J-724R <jjpernian@gmail.com>
2023-04-14 18:42:45 -0400

    Update with examples

--- JSON.py
+++ JSON.py
@@ -1,5 +1,3 @@
-import json
-
 # the #! json.loads 
 # Converts json to python
 # turns a string into a json object datastructure
@@ -8,4 +6,29 @@ import json
 # Converts form python to json
 # methods takes an object and returns a String
 
+#! Exercise
+# Convert a Python object containing all the legal data types:
+import json
 
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+
+# The json.dumps() method has parameters to make it easier to read the result:
+
+print(json.dumps(x, indent=4))
+
+# The json.dumps() method has parameters to order the keys in the result:
+
+json.dumps(x, indent=2, sort_keys=True)
